da96edecb69854afad846e2d4eee63ad74e03bf7
IsaacManzo <isaacemanzo51@gmail.com>
2022-08-22 17:39:43 -0300

    Añadi-archivo-Users.js

---
+++ api/models/Users.js
@@ -0,0 +1,60 @@
+const S = require("sequelize");
+const db = require("../db/db");
+const bcrypt = require("bcrypt");
+
+class User extends S.Model {
+  hash(plainPW, salt) {
+    return bcrypt.hash(plainPW, salt);
+  }
+  validatePassword(plainPW) {
+    return this.hash(plainPW, this.salt).then(
+      (newHash) => newHash === this.password
+    );
+  }
+}
+
+User.init(
+  {
+    email: {
+      type: S.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      },
+    },
+    firstName: {
+      type: S.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: S.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: S.STRING,
+      allowNull: false,
+    },
+    salt: {
+      type: S.STRING,
+    },
+    age: {
+      type: S.NUMBER,
+      allowNull: false,
+    },
+    admin: {
+      type: S.BOOLEAN,
+      allowNull: false,
+    },
+  },
+  { sequelize: db, modelName: "user" }
+);
+
+User.addHook("beforeCreate", (user) => {
+  const salt = bcrypt.genSaltSync();
+  user.salt = salt;
+  return user.hash(user.password, user.salt).then((hash) => {
+    user.password = hash;
+  });
+});
+
+module.exports = User;
---
+++ api/models/index.js
@@ -0,0 +1,5 @@
+const User = require("./Users")
+const Product = require("./Products")
+const Cart = require("./Carts")
+
+module.exports = { User, Product, Cart }
