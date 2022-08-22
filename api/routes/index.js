b0dd5a699fe27dfbbd9ded8e77d8b5e99ff0849b
Jtendersen <jtendersen@hotmail.com>
2022-08-22 18:09:08 -0300

    agrego index.js de routes

---
+++ api/routes/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const users = require("./users.js");
+const products = require("./products.js");
+const carts = require("./carts.js");
+
+const router = express.Router();
+
+router.use("/users", users);
+router.use("/products", products);
+router.use("/carts", carts);
+
+module.exports = router;
