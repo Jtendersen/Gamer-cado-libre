370514a0efb22e86880f0d715261fc25cd0075d8
IsaacManzo <isaacemanzo51@gmail.com>
2022-08-29 16:35:54 -0300

    nuevo

--- front/src/components/admin_panel/Admin_users.jsx
+++ front/src/components/admin_panel/Admin_users.jsx
@@ -1,1 +1,13 @@
-import axios from "axios";
+import React from 'react'
+import Navbar from "../../commons/Navbar"
+import Users from "../Users"
+const Admin_users = () => {
+  return (
+    <>
+    <Navbar/>
+    <Users/>
+</>
+  )
+}
+
+export default Admin_users
