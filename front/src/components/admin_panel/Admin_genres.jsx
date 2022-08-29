12f7f257edf8c54a70e0f0ef88ccde62d8b0e3a5
russinh0 <nahuel.russo18@gmail.com>
2022-08-29 12:43:26 -0300

    backup admin panel (genres) buttons

--- front/src/components/admin_panel/Admin_genres.jsx
+++ front/src/components/admin_panel/Admin_genres.jsx
@@ -10,22 +10,38 @@ const Admin_genres=()=>{
         console.log(data.data)
         setGenre(data.data)})
     },[])
-    const remove_genre=(genre)=>{
-        axios.delete("http://localhost:3001/api/genres")
+    const remove_genre=(genreID,genreName)=>{
+        axios.delete("http://localhost:3001/api/genres",genreID)
+        .then((data)=>{
+            if(data.status===404)alert("The selected genre wasn't finded")
+            alert(`The genre ${genreName} was deleted`)})
+    }
+    const edit_genre=(genreID,newGenreName)=>{
+        axios.put(`http://localhost:3001/api/genres/${genreID}`,{genre:newGenreName})
+        .then(()=>{
+            if(data.status===404) alert('syntax error')
+            alert(`The name genre was changed to "${newGenreName}"`)})
+    }
+    const add_genre=(genreName)=>{
+        axios.create(`http://localhost:3001/api/genres`,{genre:genreName})
+        .then((data)=>{
+            if(data.status===404)alert('syntax error')
+            alert(`The genre ${genreName} was created`)
+        })
     }
     return(
         <>
         <Navbar/>
         <Stack spacing={4} >
-            <Button>ADD</Button>
+            <Button onClick={()=>{add_genre("TEST")}} >ADD</Button>
         {
         genres.map((genre,id)=>{
            return (<li key={id}>
                 {genre.genre}
             <ButtonGroup size='small'>
                 <br/>
-                <Button>EDIT</Button>
-                <Button color='error' onClick={()=>remove_genre} >REMOVE</Button>
+                <Button onClick={()=>{edit_genre(genre.id,genre.genre)}} >EDIT</Button>
+                <Button color='error' onClick={()=>{remove_genre(genre.id,"TEST")}} >REMOVE</Button>
             </ButtonGroup>
             </li>)
         })}
