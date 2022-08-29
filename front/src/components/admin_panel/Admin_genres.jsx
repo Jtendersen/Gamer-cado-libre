957a69388cba898a0078817d7ccc7ac49dc83f9e
russinh0 <nahuel.russo18@gmail.com>
2022-08-29 12:16:32 -0300

    backup admin_genres panel

--- front/src/components/admin_panel/Admin_genres.jsx
+++ front/src/components/admin_panel/Admin_genres.jsx
@@ -1,18 +1,35 @@
+import { Button, ButtonGroup, Stack } from "@mui/material";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Navbar from '../../commons/Navbar'
 
 const Admin_genres=()=>{
-    const [genre,setGenre]=useState([])
+    const [genres,setGenre]=useState([])
     useEffect(()=>{
         axios.get("http://localhost:3001/api/genres").then(data=>{
-        console.log(data.data)    
+        console.log(data.data)
         setGenre(data.data)})
-    },{})
-    console.log(genre)
+    },[])
+    const remove_genre=(genre)=>{
+        axios.delete("http://localhost:3001/api/genres")
+    }
     return(
         <>
         <Navbar/>
+        <Stack spacing={4} >
+            <Button>ADD</Button>
+        {
+        genres.map((genre,id)=>{
+           return (<li key={id}>
+                {genre.genre}
+            <ButtonGroup size='small'>
+                <br/>
+                <Button>EDIT</Button>
+                <Button color='error' onClick={()=>remove_genre} >REMOVE</Button>
+            </ButtonGroup>
+            </li>)
+        })}
+        </Stack>
         Hola
         </>
     )
