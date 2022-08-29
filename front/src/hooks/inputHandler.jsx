25e795631d4306cb3e3b62b7b895bf4920caf221
russinh0 <nahuel.russo18@gmail.com>
2022-08-29 16:49:45 -0300

    CRUD/genres funcionalidad terminado (diseño pending)

---
+++ front/src/hooks/inputHandler.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+
+
+
+const Input_handler=()=>{
+    const [value,setInput]=useState('')
+    const onChange=(e)=>{
+        setInput(e.target.value)
+    }
+    return {value,onChange}
+}
+export default Input_handler
