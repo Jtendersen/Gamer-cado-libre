7d9dcb98277cfa1f6c935775aba7df4a8e5ffb9e
russinh0 <nahuel.russo18@gmail.com>
2022-08-30 17:23:04 -0300

    agrego hook en review para promediar ratings

--- api/models/Reviews.js
+++ api/models/Reviews.js
@@ -1,5 +1,6 @@
 const { DataTypes, Model } = require("sequelize");
 const sequelize = require("../db/db");
+const Product = require("./Products");
 
 class Review extends Model {}
 
@@ -21,5 +22,21 @@ Review.init(
     modelName: "review",
   }
 );
+
+//SACA EL PROMEDIO DE TODOS LOS RATING REVIEW DE EL PRODUCTO EN EL QUE SE AGREGO LA REVIEW.
+Review.addHook('afterCreate',(review)=>{
+  Review.findAll({where:{productId:review.productId},
+    attributes:['productId','rating'],
+    include:{model:Product,
+      attributes:['id','valoration']}})
+  .then(allReviews=>{
+    var avgRating=0
+    allReviews.forEach((userReview,i)=>{
+      if(userReview.rating)avgRating+=userReview.rating
+      if(!allReviews[i+1])avgRating/=i
+    })
+    Product.update({valoration:avgRating},{where:{id:allReviews[0].product.id}})
+  })
+})
 
 module.exports = Review;
