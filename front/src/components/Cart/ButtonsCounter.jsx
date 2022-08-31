d1c0dcf5ca37431c7b38c6892f4c925f6a4b5e4d
Emmanuel <emmanuel.dacal@gmail.com>
2022-08-31 20:54:27 -0300

    tema y otras magias

--- front/src/components/Cart/ButtonsCounter.jsx
+++ front/src/components/Cart/ButtonsCounter.jsx
@@ -1,36 +1,32 @@
 import React from 'react'
-import { useState } from 'react';
+import { useState, useEffect} from 'react';
 import { ButtonGroup, Button } from '@mui/material';
-//import { useDispatch } from 'react-redux';
-//import {cartItemQuantity} from "../../state/cart"
+import { useDispatch, useSelector } from 'react-redux';
+import {cartItemQuantity} from "../../state/cart"
 
 const ButtonsCounter = (props) => {
 
-    //const dispatch = useDispatch()
+    const dispatch = useDispatch()
+    const user = useSelector((state) => state.user);
 
     const [counter, setCounter] = useState(1);
+    
+    useEffect(() => {
+      dispatch(cartItemQuantity({
+        userId:user.id,
+        productId:props.itemId,
+        quantity:counter}))
+      
+    }, [counter]);
 
 return (
    <ButtonGroup size="small" aria-label="small outlined button group">
 
-        <Button onClick={()=> 
-        {setCounter(counter=>counter+1)
-          console.log(typeof(props.quantity))
-        console.log("COUNTER", counter)
-        // dispatch(cartItemQuantity({
-        //   itemId:props.itemId,
-        //   counter:counter}))
-        }}>+</Button>
+    <Button onClick={()=>{setCounter(counter+1)}}>+</Button>
 
      {<Button disabled>{counter}</Button>}
 
-      {<Button disabled={counter <= 1} onClick={() => {
-        setCounter(counter => counter - 1)
-        console.log("COUNTER", counter)
-        // dispatch(cartItemQuantity({
-        //   itemId:props.itemId,
-        //   counter:counter}))
-        }}>-</Button>}
+    {<Button disabled={counter <= 1} onClick={() => {setCounter(counter -1)}}>-</Button>}
 
   </ButtonGroup>
 )
