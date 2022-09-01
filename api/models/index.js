6852769389ca5885d6514366b9c961eca56a88b3
Jtendersen <jtendersen@hotmail.com>
2022-09-01 11:55:30 -0300

    Se vuelve a version original de index.js en models

--- api/models/index.js
+++ api/models/index.js
@@ -12,7 +12,7 @@ Genre.hasMany(Product);
 Order.belongsTo(User, { foreignKey: "userId" });
 User.hasMany(Order);
 Cart.belongsTo(Order, { foreignKey: "orderId" });
-Order.hasMany(Cart);
+// Cart.hasOne(Order);
 Cart.belongsTo(Product, { foreignKey: "productId" });
 Cart.belongsTo(User);
 User.hasMany(Cart);
