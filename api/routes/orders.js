0d37720c24c094a1c538a7a20f503512d1a887cc
Jtendersen <jtendersen@hotmail.com>
2022-08-31 23:25:55 -0300

    Ruta de orders terminada

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,55 +1,61 @@
 const express = require("express");
 const { Op } = require("sequelize");
+const { validateAuth } = require("../middlewares/auth");
 const { Cart, Order } = require("../models");
 const sendEmail = require("../utils/index.js");
 
 const router = express.Router();
 
 // Ruta para cuando el cliente confirma un pedido.
-router.post("/:userId", (req, res, next) => {
-  //ver de recibir por cookies el usuario
-  //recibe un req.body con el sig formato:
-  /*
-        {
-          nombre: nombrecompleto
-            paymentMethod: pMethod,
-            sendingAddress: sAddress
-        }
-    */
-
+router.post("/", validateAuth, (req, res, next) => {
   Order.create({
-    userId: req.params.userId,
+    userId: req.user.id,
     paymentMethod: req.body.paymentMethod,
     sendingAddress: req.body.sendingAddress,
     state: "created",
   }).then((response) => {
     const orderId = response.dataValues.id;
-    // console.log("ESTE ES EL RESPONSE DEL CREATE ORDER", response);
+    const totalCartsPrice = response.dataValues.totalOrderPrice;
     Cart.findAll({
       where: {
-        [Op.and]: [{ userId: req.params.userId }, { state: "pending" }],
+        [Op.and]: [{ userId: req.user.id }, { state: "pending" }],
       },
-    }).then((cartOrder) => {
-      //   console.log("ESTE ES EL ARRAY DE CARTS", cartOrder);
-      cartOrder.map((cart) => {
-        Cart.findByPk(cart.dataValues.id).then((cartToUpdateState) => {
-          cartToUpdateState.update({ state: "complete" });
-          cartToUpdateState.setOrder(orderId);
+    })
+      .then((cartOrder) => {
+        cartOrder.map((cart) => {
+          Cart.findByPk(cart.dataValues.id).then((cartToUpdateState) => {
+            cartToUpdateState.update({ state: "complete" });
+            cartToUpdateState.setOrder(orderId);
+          });
         });
-      });
-    });
-  });
-  //aca habria que hacer querys para enviar el pedido completo al front y por mail
-  sendEmail(
-    "jtendersen@hotmail.com",
-    "jtendersen@gmail.com",
-    "Amiguito",
-    "99565446546",
-    "<p>Superlujo1</p><p>SuperLujo2</p><p>Superlujo3</p>",
-    `${req.body.recieverName}`
-  );
+      })
+      .then(() => {
+        //armamos la lista de productos para mostrar en el mail:
+        {
+          let productArray = req.body.cart.map((product) => {
+            return `
+    <tr >
+        <td>${product.product.name}</td>
+        <td>${product.quantity}</td>
+        <td>${product.product.price}</td>
+        <td>${product.totalPrice}</td>
+      </tr>
+    `;
+          });
+          let productos = productArray.join("");
 
-  res.sendStatus(201);
+          sendEmail(
+            req.user.email,
+            req.user.firstName,
+            orderId,
+            productos,
+            req.body.recieverName,
+            totalCartsPrice
+          );
+        }
+      });
+    res.send(`Order ${orderId} created successfully`);
+  });
 });
 
 module.exports = router;
