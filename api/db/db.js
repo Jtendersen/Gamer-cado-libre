f4662f302207424a05c9d239999cf38f0610aa66
Jtendersen <jtendersen@hotmail.com>
2022-12-01 21:00:03 -0300

    server config port

--- api/db/db.js
+++ api/db/db.js
@@ -1,8 +1,13 @@
 const Sequelize = require("sequelize");
-const db = new Sequelize("e-commerce", null, null, {
-  host: "localhost",
-  dialect: "postgres",
-  logging: false,
-});
+const db = new Sequelize(
+  process.env.DB_NAME || "e-commerce",
+  process.env.DB_USER || null,
+  process.env.DB_PASSWORD || null,
+  {
+    host: process.env.DB_HOST || "localhost",
+    dialect: "postgres",
+    logging: false,
+  }
+);
 
 module.exports = db;
